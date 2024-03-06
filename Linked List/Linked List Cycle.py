506228c1e8617ce3a09dfd06a3ed7880a77e1d1a
Ibrahim <zemb34@gmail.com>
2024-03-06 14:48:17 +0000

    "Added 141. Linked List Cycle"

---
+++ Linked List/Linked List Cycle.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool: 
+#         visited = set()
+#         current = head
+
+#         while current:
+#             if current in visited:
+#                 return True
+#             visited.add(current)
+#             current = current.next
+#         return False
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool: 
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        
+        return False
